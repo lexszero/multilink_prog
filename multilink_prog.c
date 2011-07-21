01a3ff49e712467264b46161a786791b85e03a48
Alex Ignatov <lexszero@gmail.com>
2011-07-21 04:03:30 +0400

    fix .gitignore, some work in main()

--- multilink_prog.c
+++ multilink_prog.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include "unit_cfz.h"
 
-int main() {
+int main(int argc, char **argv) {
 	unit_cfz_load("lib/unit_cfz_v1.so");
 	
 	DEBUG("%s\n", version());
+
 	DEBUG("Enumerated %i ports\n", get_enumerated_number_of_ports(PortType_USBMultilink));
+
+	char *port_desc = get_port_descriptor(PortType_USBMultilink, 1);
+	DEBUG("Using port %s\n", port_desc);
+
+	if (open_port(PortType_USBMultilink, 1) != 0) {
+		DEBUG("open_port failed, check_critical_error = %x\n", check_critical_error());
+		return 1;
+	}
+
+	close_port();
+	unit_cfz_unload();
+
+	return 0;
 }
